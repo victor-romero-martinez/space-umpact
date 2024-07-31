a7431c35afd0d567774f5d78e7f7eacdfad86401
victor-romero-martinez <victordav147@gmail.com>
2024-07-31 16:23:43 -0400

    update: add animation when entering

--- scripts/hud_heart.gd
+++ scripts/hud_heart.gd
@@ -3,21 +3,29 @@ extends Node2D
 class_name HudHealth
 
 @export var heart_compnent: PackedScene
-@export var initial_position: Vector2 = Vector2(8.0, 0)
+@export var position_in_game: Marker2D
 
 var player_heart = Global.player_heart
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	if heart_compnent:
+	if heart_compnent and position_in_game:
 		for h in player_heart:
 			var heart = heart_compnent.instantiate()
-			heart.position = initial_position * h #NOTE: (x * i, y * i)
+			heart.position.x = position.x + 8.0 * h
 			add_child(heart)
+		_show_health()
 	else:
-		push_error('PackedScene is missing')
+		push_error('PackedScene or InitialHealthPosition is missing')
 	
+	
+func _show_health():
+	var t = create_tween()
+	t.tween_property(self, 'position', position_in_game.position, .45)\
+		.set_trans(Tween.TRANS_LINEAR)\
+		.set_ease(Tween.EASE_OUT)
+
 
 func remove_heart():
 	if get_child_count() > 0:
--- scripts/player.gd
+++ scripts/player.gd
@@ -8,33 +8,39 @@ extends CharacterBody2D
 
 @export_group('Bullet Settings')
 @export var bullet_scene: PackedScene
+## how many bullets can shots
 @export var bullet_by_shoot: int = 3
+## how many time wait for the next shooting
 @export var wait_seconds: float = .5
 
 @export_group('Settings')
 @export var speed = 40.0
+@export var respawn: Marker2D
 @export var explotion_scene: PackedScene
 
 @onready var hud_health = $"../../Hud/HudHealth" as HudHealth
 
 var global
-const RESPAWN = Vector2(6, 38)
 var _can_shoot: bool = true
+var _immunity = true
+var _can_move = false
 
 
 func _ready():
 	if not bullet_scene\
-	or not explotion_scene:
+	or not explotion_scene\
+	or not respawn:
 		push_error('Some PackedScene is missing')
 		return
 	
 	global = Global
-	global_position = RESPAWN
 	_animation_spawn()
+	_start_combat()
 
 func _physics_process(delta):
-	_move(delta)
-	move_and_slide()
+	if _can_move:
+		_move(delta)
+		move_and_slide()
 	
 	#INFO: received damage and restart initioal position
 	if is_on_wall() or is_on_floor():
@@ -48,25 +54,39 @@ func _move(delta):
 	var direction = Input.get_vector('ui_left', 'ui_right', 'ui_up', 'ui_down')
 	velocity = direction.normalized() * speed
 
-	#WARNING: hace un pequeño movimineto
-	position += velocity * delta
-	position = position.clamp(Vector2.ZERO, (Global.screen_size - Vector2(21.0, 15.0)))
+	if _can_move:
+		# limits movement on the screen
+		#WARNING: hace un pequeño movimineto
+		position += velocity * delta
+		position = position.clamp(Vector2.ZERO, (Global.screen_size - Vector2(21.0, 15.0)))
+
+# enter from outside
+func _start_combat():
+	var t = create_tween()
+	t.tween_property(self, 'global_position', respawn.position, .45)\
+		.set_trans(Tween.TRANS_LINEAR)\
+		.set_ease(Tween.EASE_OUT)
+	t.tween_callback(func (): _can_move = true)
 	
-# apply damage and reset position
+	
+# apply damage and reset position when not immunity
 func make_boom():
-	_apply_explotion()
-	_animation_spawn()
-	
-	global_position = RESPAWN
-	
-	global.take_damage() #NOTE: base 1
-	hud_health.remove_heart() #NOTE: base index 0
+	if not _immunity:
+		_apply_explotion()
+		_animation_spawn()
+		
+		global_position = respawn.position
+		
+		global.take_damage() #NOTE: base 1
+		hud_health.remove_heart() #NOTE: base index 0
 
 
 # apply animation
 func _animation_spawn():
+	_immunity = true
 	$AnimatedSprite2D.play('respawn')
 	await get_tree().create_timer(2.0).timeout
+	_immunity = false
 	$AnimatedSprite2D.play('default')
 
 
@@ -95,8 +115,3 @@ func _shoot_handle():
 	await get_tree().create_timer(wait_seconds).timeout
 	_can_shoot = true
 
-
-#WARNING: don't forget to select Collitions layer
-#func _on_player_hit_box_area_entered(area):
-	#if area is EnemyHitBox or area is BulletHitBox:
-		#make_boom()
