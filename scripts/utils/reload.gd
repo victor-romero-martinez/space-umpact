1bf9aa30bcba41cad12186516f453413ea2d933a
victor-romero-martinez <victordav147@gmail.com>
2024-08-14 18:24:31 -0400

    feat: add random guns generator

--- scripts/utils/reload.gd
+++ scripts/utils/reload.gd
@@ -6,17 +6,27 @@ class_name ReloadItem
 enum TBullet { rocket, laser_h, laser_v }
 
 @export var bullet_type: TBullet
+@export var random_type: bool = false
+
 @onready var global = Global
 
+var _gun_name: String
+
 func _ready():
-	%AnimatedSprite.play(TBullet.keys()[bullet_type])
+	if random_type:
+		var random_idx = randi_range(0, (TBullet.size() - 1))
+		_gun_name = TBullet.find_key(random_idx)
+	else:
+		_gun_name = TBullet.keys()[bullet_type]
+		
+	%AnimatedSprite.play(_gun_name)
 	
 
 func _on_area_2d_area_entered(area):
 	if area is PlayerCollectorItem:
-		if global.player_arsenal.has(TBullet.keys()[bullet_type]): return
+		if global.player_arsenal.has(_gun_name): return
 		
-		area.received_item(TBullet.keys()[bullet_type])
+		area.received_item(_gun_name)
 		queue_free()
 			
 
