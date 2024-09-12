1197308d08e29f9cb1955da07ffb5156e2a6be18
victor-romero-martinez <victordav147@gmail.com>
2024-09-12 09:03:00 -0400

    update: score outline

--- scripts/utils/score.gd
+++ scripts/utils/score.gd
@@ -5,8 +5,7 @@ extends Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	text = str(Global.game_data.score).pad_zeros(7)
-	material.set("shader_parameter/outline_color", Color(Global.game_data.theme[1]))
-	material.set("shader_parameter/sprite_tint", Color(Global.game_data.theme[2]))
+	add_theme_color_override("font_outline_color", Color(Global.game_data.theme[1]))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
