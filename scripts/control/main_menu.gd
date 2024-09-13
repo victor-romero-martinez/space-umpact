a2cf189201c3571b74be51197f30bf158070432a
victor-romero-martinez <victordav147@gmail.com>
2024-09-13 11:53:01 -0400

    rm: button reset

--- scripts/control/main_menu.gd
+++ scripts/control/main_menu.gd
@@ -33,6 +33,7 @@ func _ready():
 	
 func _on_new_game_pressed():
 	$Beep1.play()
+	global.restart_game()
 	$Beep1.finished.connect(func (): get_tree().change_scene_to_file("res://scenes/level_1.tscn"))
 	
 
